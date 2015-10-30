abf1f11b92dda833bd252725f44109f93fb27d99
onlyjackma <majian699@163.com>
2015-10-30 18:03:34 +0800

    add some property for user and add one to one char for server

--- chat/chatServer.js
+++ chat/chatServer.js
@@ -3,6 +3,7 @@ var User = require('./user');
 
 var server  = net.createServer();
 server.listen(9000);
+var clients = {};
 
 server.on('connection',function(client){
 	console.log("Client connect from :"+client.remoteAddress +' and '+client.remotePort);
@@ -11,12 +12,38 @@ server.on('connection',function(client){
 		console.log("data:"+data);
 		var tobj = JSON.parse(data);
 		if(tobj['type'] != undefined && tobj['type'] == 'login'){
-			//var user = new User;
-			//user.setId(tobj['id']);
+			var user = new User;
+			user.setId(tobj['id']);
+			user.setName(tobj['name']);
+			user.setSocket(client);
 			console.log(tobj['type']);
+		
+			clients[tobj['id']] = user;
+		
+			client.write("login success!");
+		}else if(tobj['type'] != undefined && tobj['type'] == 'msg'){
+			var dest_usr_id = tobj['dest_id'];
+			var dest_usr;
+			if(dest_usr_id){
+				dest_usr = clients[dest_usr_id];
+			}else{
+				client.write("who do you want to send msg to!");
+				return;
+			}
+			if(dest_usr){
+				var dest_sock = dest_usr.getSocket();
+				if(dest_sock.writable){
+					dest_sock.write(tobj['name']+" say :"+tobj['msg']);
+				}else{
+					client.write(dest_usr_id+" is off line!");
+				}
+			}
+			
 		}
-		client.write(data.toString());
+		
+		//client.write(data.toString());
 	});
+
 //	setInterval(function(){
 //		if(client.writable)
 //		client.write("ni lai da wo sa!");
--- chat/user.js
+++ chat/user.js
@@ -1,11 +1,26 @@
 function User(){
 	var name;
 	var id;
+	var sock;
+
 	this.setId = function(pid){
 		id = pid;
 	}
 	this.getId = function(){
 		return id;
+	}
+	this.setName = function(pname){
+		name = pname;
+	}
+	this.getName = function(){
+		return name;
+	}
+
+	this.setSocket = function(psock){
+		sock = psock;
+	}
+	this.getSocket = function(){
+		return sock;
 	}
 }
 
